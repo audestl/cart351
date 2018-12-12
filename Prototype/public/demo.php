6fb1ba667edaaa08b0ab03a6e7997016767874f5
Audrey-Laure St-Louis <43054179+audestl@users.noreply.github.com>
2018-12-12 14:34:27 -0500

    Add files via upload

---
+++ Prototype/public/demo.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Personal Journal for Confidences </title>
+<!-- get JQUERY -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+<style>
+@import url('https://fonts.googleapis.com/css?family=Monoton');
+</style>
+<link rel="stylesheet" type="text/css" href="css/demostyle.css">
+</head>
+<body>
+
+<div class="centered">
+
+  <div class="split left">
+    <div class="subtil"><a href="" style="text-decoration:none; padding-left:10px; ">ABOUT</a></div>
+
+
+      <h1 style="padding-top: 27%;"><a href="inspiration_choice.php" style="text-decoration:none; color:#302745; ">GET INSPIRED</a></h1>
+      <p id="results">
+
+</p>
+    </div>
+  </div>
+<div class="centered">
+  <div class="split right">
+
+
+      <h1 style="padding-top: 28%;"><a href="inspiration_confession.php" style="text-decoration:none; color:#E6A67F">INSPIRE OTHERS</a></h1>
+
+    </div>
+  </div>
+
+
+
+
+</body>
+
+</html>
